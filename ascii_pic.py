c7d31ee33c660474f52c95fc225b08713a72bc95
codrutiftode <codriniftode@gmail.com>
2022-05-15 17:17:34 +0100

    Modularise render alphabet

--- ascii_pic.py
+++ ascii_pic.py
@@ -4,110 +4,73 @@ from PIL import Image, ImageDraw, ImageFont, ImageCms
 import matplotlib.pyplot as plt
 import math
 import cv2
-
-alphabet = "~/#@0$!&*"
+import time
+import json
+from render_alphabet import ALPHABET_DIR, Alphabet
 
 
 def main():
     print("Rendering...")
     image_path = sys.argv[1]
-    alphabet_path = "./alphabet/alphabet.png"
-    asciify(image_path, alphabet_path)
+    with Alphabet(ALPHABET_DIR) as alphabet:
+        start = time.time()
+        asciify(image_path, alphabet)
+        end = time.time()
     print("Finished render.")
+    print("Total time: %0.3f" % (end - start))
 
 
-def random_char():
-    """ Returns a random ASCII draw char """
-    return alphabet[math.floor(np.random.rand() * len(alphabet))]
-
-
-def asciify_legacy(image_path):
+def asciify(image_path, alphabet):
     """ Takes an image path and draws it with ASCII characters """
+    # Unpack alphabet data
+    dx = alphabet.meta["char_width"]
+    dy = alphabet.meta["char_height"]
+    alphabet_chars = alphabet.meta["chars"]
     background_color = (255, 255, 255)
 
     with Image.open(image_path) as image_in:
         np_image = np.array(image_in)
         image_width, image_height = image_in.size
+
+        # Create output image
         image_out = Image.new("RGB", image_in.size, background_color)
-
-        font_size = 25
-        font_x_factor = 3200
-        font_y_factor = 1720
-        font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
-
         draw_context = ImageDraw.Draw(image_out)
 
-        dx = round(image_width * font_size / font_x_factor)
-        dy = round(image_height * font_size / font_y_factor)
-
+        # Draw pixelated output background
         for y in range(0, image_height, dy):
             for x in range(0, image_width, dx):
                 tile = np_image[y:y+dy, x:x+dx, :]
                 average_rgb = np.mean(tile, axis=(0, 1))
                 color = np.asarray(np.round(average_rgb), dtype="int")
-                text_bg_color = color // 3
 
                 draw_context.rectangle(
-                    (x, y, x+dx, y+dy), fill=tuple(text_bg_color))
-                draw_context.text((x, y), random_char(),
-                                  font=font, fill=tuple(color))
+                    (x, y, x+dx, y+dy), fill=tuple(color))
 
-        image_out.show()
+        # Use alphabet to create luminance mask
+        np_alphabet = np.array(alphabet.image.convert("L"))
+        split_alphabet = np.array(
+            np.split(np_alphabet, np_alphabet.shape[1] // dx, axis=1))
+        countx = image_width // dx + 1
+        county = image_height // dy + 1
+        indices_mat = np.floor(np.random.rand(
+            county, countx) * len(alphabet_chars))
+        L_alphabet_blocks = split_alphabet[np.array(
+            indices_mat, dtype="int")]
+        L_alphabet = np.block([[col for col in row]
+                               for row in L_alphabet_blocks])
 
+        # Add characters by weight-averaging the luminance channel with the luminance mask
+        hls_image_out = cv2.cvtColor(
+            np.array(image_out), cv2.COLOR_RGB2LAB)
+        weight_original = 1
+        weight_mask = 2
+        hls_image_out[:, :, 0] = (hls_image_out[:, :, 0] * weight_original +
+                                  L_alphabet[0:image_height, 0:image_width] / 255 * 100 * weight_mask) // (weight_original + weight_mask)
 
-def asciify(image_path, alphabet_path):
-    """ Takes an image path and draws it with ASCII characters """
-    background_color = (255, 255, 255)
-
-    with Image.open(alphabet_path) as image_alphabet:
-        with Image.open(image_path) as image_in:
-            np_image = np.array(image_in)
-            image_width, image_height = image_in.size
-
-            # Create output image
-            image_out = Image.new("RGB", image_in.size, background_color)
-            draw_context = ImageDraw.Draw(image_out)
-
-            # Init font
-            font_size = 25
-            font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
-            dx, dy = font.getsize("#")
-
-            # Draw pixelated output background
-            for y in range(0, image_height, dy):
-                for x in range(0, image_width, dx):
-                    tile = np_image[y:y+dy, x:x+dx, :]
-                    average_rgb = np.mean(tile, axis=(0, 1))
-                    color = np.asarray(np.round(average_rgb), dtype="int")
-
-                    draw_context.rectangle(
-                        (x, y, x+dx, y+dy), fill=tuple(color))
-
-            # Use alphabet to create luminance mask
-            np_alphabet = np.array(image_alphabet.convert("L"))
-            split_alphabet = np.array(
-                np.split(np_alphabet, np_alphabet.shape[1] // dx, axis=1))
-            countx = image_width // dx + 1
-            county = image_height // dy + 1
-            indices_mat = np.floor(np.random.rand(
-                county, countx) * len(alphabet))
-            L_alphabet_blocks = split_alphabet[np.array(
-                indices_mat, dtype="int")]
-            L_alphabet = np.block([[col for col in row]
-                                  for row in L_alphabet_blocks])
-
-            # Add characters by weight-averaging the luminance channel with the luminance mask
-            hls_image_out = cv2.cvtColor(
-                np.array(image_out), cv2.COLOR_RGB2LAB)
-            weight_original = 1
-            weight_mask = 2
-            hls_image_out[:, :, 0] = (hls_image_out[:, :, 0] * weight_original +
-                                      L_alphabet[0:image_height, 0:image_width] / 255 * 100 * weight_mask) // (weight_original + weight_mask)
-
-            # Create final image
-            final_image = Image.fromarray(cv2.cvtColor(
-                hls_image_out, cv2.COLOR_LAB2RGB), mode="RGB")
-            final_image.show()
+        # Create final image
+        final_image = Image.fromarray(cv2.cvtColor(
+            hls_image_out, cv2.COLOR_LAB2RGB), mode="RGB")
+        final_image.show()
 
 
 if __name__ == "__main__":
--- render_alphabet.py
+++ render_alphabet.py
@@ -1,25 +1,62 @@
+import json
 from PIL import ImageDraw, Image, ImageFont
 import os
+import json
 
-alphabet = "~/#@0$!&*"
-output_dir = "./alphabet/"
-font_size = 25
+ALPHABET_DIR = "./alphabet/"
+ALPHABET_PNG = "alphabet.png"
+ALPHABET_META = "alphabet.json"
 
-# Create output dir
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
+def render_alphabet():
+    alphabet = "~/#@0$!&*"
+    font_size = 25
 
-# Font config
-font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
-image_width, image_height = font.getsize("#")
-image_size = (image_width * len(alphabet), image_height)
+    # Create output dir
+    if not os.path.isdir(ALPHABET_DIR):
+        os.mkdir(ALPHABET_DIR)
 
-# Render alphabet
-print("Rendering alphabet...")
-image_out = Image.new("RGB", image_size, (90, 90, 90))
-draw_context = ImageDraw.Draw(image_out)
-for (i, char) in enumerate(alphabet):
-    draw_context.text((i * image_width, 0), text=char, font=font, fill=(255, 255, 255))
+    # Font config
+    font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
+    image_width, image_height = font.getsize("#")
+    image_size = (image_width * len(alphabet), image_height)
 
-image_out.save(output_dir + "alphabet.png")
-print("Render done.")
+    # Render alphabet
+    print("Rendering alphabet...")
+    image_out = Image.new("RGB", image_size, (90, 90, 90))
+    draw_context = ImageDraw.Draw(image_out)
+    for (i, char) in enumerate(alphabet):
+        draw_context.text((i * image_width, 0), text=char,
+                        font=font, fill=(255, 255, 255))
+
+    image_out.save(ALPHABET_DIR + ALPHABET_PNG)
+
+    # Write alphabet metadata
+    with open(ALPHABET_DIR + ALPHABET_META, "w") as alphabet_meta:
+        alphabet_meta.write(json.dumps({
+            "char_width": image_width,
+            "char_height": image_height,
+            "chars": alphabet
+        }))
+
+    print("Render done.")
+
+
+class Alphabet():
+    """ Loads and unloads an alphabet and its metadata """
+    def __init__(self, alphabet_dir):
+        self.image_path = alphabet_dir + ALPHABET_PNG
+        self.meta_path = alphabet_dir + ALPHABET_META
+
+    def __enter__(self):
+        self.image = Image.open(self.image_path)
+        with open(self.meta_path) as meta_file:
+            self.meta = json.loads(meta_file.read())
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.image.close()
+
+
+# Main function behaviour
+if __name__ == "__main__":
+    render_alphabet()
