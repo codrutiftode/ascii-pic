12d79e2e10a382ce10fdd3109a722aca00ef2a61
codrutiftode <codriniftode@gmail.com>
2022-05-15 16:22:31 +0100

    Improve readability. Add comments. Formatter. Helper prints.

--- ascii_pic.py
+++ ascii_pic.py
@@ -9,10 +9,12 @@ alphabet = "~/#@0$!&*"
 
 
 def main():
+    print("Rendering...")
     image_path = sys.argv[1]
     alphabet_path = "./alphabet/alphabet.png"
     asciify(image_path, alphabet_path)
-    
+    print("Finished render.")
+
 
 def random_char():
     """ Returns a random ASCII draw char """
@@ -27,12 +29,12 @@ def asciify_legacy(image_path):
         np_image = np.array(image_in)
         image_width, image_height = image_in.size
         image_out = Image.new("RGB", image_in.size, background_color)
-        
+
         font_size = 25
         font_x_factor = 3200
         font_y_factor = 1720
         font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
-        
+
         draw_context = ImageDraw.Draw(image_out)
 
         dx = round(image_width * font_size / font_x_factor)
@@ -40,13 +42,15 @@ def asciify_legacy(image_path):
 
         for y in range(0, image_height, dy):
             for x in range(0, image_width, dx):
-                tile = np_image[y:y+dy,x:x+dx,:]
+                tile = np_image[y:y+dy, x:x+dx, :]
                 average_rgb = np.mean(tile, axis=(0, 1))
                 color = np.asarray(np.round(average_rgb), dtype="int")
                 text_bg_color = color // 3
 
-                draw_context.rectangle((x, y, x+dx, y+dy), fill=tuple(text_bg_color))
-                draw_context.text((x, y), random_char(), font=font, fill=tuple(color))
+                draw_context.rectangle(
+                    (x, y, x+dx, y+dy), fill=tuple(text_bg_color))
+                draw_context.text((x, y), random_char(),
+                                  font=font, fill=tuple(color))
 
         image_out.show()
 
@@ -59,41 +63,52 @@ def asciify(image_path, alphabet_path):
         with Image.open(image_path) as image_in:
             np_image = np.array(image_in)
             image_width, image_height = image_in.size
+
+            # Create output image
             image_out = Image.new("RGB", image_in.size, background_color)
             draw_context = ImageDraw.Draw(image_out)
-            
+
+            # Init font
             font_size = 25
             font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
             dx, dy = font.getsize("#")
 
+            # Draw pixelated output background
             for y in range(0, image_height, dy):
                 for x in range(0, image_width, dx):
-                    tile = np_image[y:y+dy,x:x+dx,:]
+                    tile = np_image[y:y+dy, x:x+dx, :]
                     average_rgb = np.mean(tile, axis=(0, 1))
                     color = np.asarray(np.round(average_rgb), dtype="int")
-                    
-                    draw_context.rectangle((x, y, x+dx, y+dy), fill=tuple(color))
-                    # draw_context.text((x, y), random_char(), font=font, fill=tuple(color))
+
+                    draw_context.rectangle(
+                        (x, y, x+dx, y+dy), fill=tuple(color))
 
             # Use alphabet to create luminance mask
             np_alphabet = np.array(image_alphabet.convert("L"))
-            split_alphabet = np.array(np.split(np_alphabet, np_alphabet.shape[1] // dx, axis=1))
-            countx = image_width // dx
-            county = image_height // dy
-            indices_mat = np.floor(np.random.rand(county, countx) * len(alphabet))
-           
-            L_alphabet_blocks = split_alphabet[np.array(indices_mat, dtype="int")]
-            L_alphabet = np.block([[col for col in row] for row in L_alphabet_blocks])
-            print(L_alphabet.shape)
+            split_alphabet = np.array(
+                np.split(np_alphabet, np_alphabet.shape[1] // dx, axis=1))
+            countx = image_width // dx + 1
+            county = image_height // dy + 1
+            indices_mat = np.floor(np.random.rand(
+                county, countx) * len(alphabet))
+            L_alphabet_blocks = split_alphabet[np.array(
+                indices_mat, dtype="int")]
+            L_alphabet = np.block([[col for col in row]
+                                  for row in L_alphabet_blocks])
 
-            # Add characters
-            hls_image_out = cv2.cvtColor(np.array(image_out), cv2.COLOR_RGB2LAB)
-            print(hls_image_out[0:10, 0:10])
-            hls_image_out[0:1064, :, 0] = (hls_image_out[0:1064, :, 0] + L_alphabet / 255 * 100 * 2) // 3
-            final_image = Image.fromarray(cv2.cvtColor(hls_image_out, cv2.COLOR_LAB2RGB), mode="RGB")
+            # Add characters by weight-averaging the luminance channel with the luminance mask
+            hls_image_out = cv2.cvtColor(
+                np.array(image_out), cv2.COLOR_RGB2LAB)
+            weight_original = 1
+            weight_mask = 2
+            hls_image_out[:, :, 0] = (hls_image_out[:, :, 0] * weight_original +
+                                      L_alphabet[0:image_height, 0:image_width] / 255 * 100 * weight_mask) // (weight_original + weight_mask)
+
+            # Create final image
+            final_image = Image.fromarray(cv2.cvtColor(
+                hls_image_out, cv2.COLOR_LAB2RGB), mode="RGB")
             final_image.show()
 
-    
 
 if __name__ == "__main__":
     main()
--- render_alphabet.py
+++ render_alphabet.py
@@ -12,13 +12,14 @@ if not os.path.isdir(output_dir):
 # Font config
 font = ImageFont.truetype("./fonts/Courier.ttf", font_size)
 image_width, image_height = font.getsize("#")
-print(image_width, image_height)
 image_size = (image_width * len(alphabet), image_height)
 
 # Render alphabet
-image_out = Image.new("RGB", image_size, (150, 150, 150))
+print("Rendering alphabet...")
+image_out = Image.new("RGB", image_size, (90, 90, 90))
 draw_context = ImageDraw.Draw(image_out)
 for (i, char) in enumerate(alphabet):
     draw_context.text((i * image_width, 0), text=char, font=font, fill=(255, 255, 255))
 
 image_out.save(output_dir + "alphabet.png")
+print("Render done.")
